ef1cafd8e9a6c87119bd50b2cd98612b7a8f3a8b
Ara <ayaghsizian@gmail.com>
2017-08-09 17:24:56 -0400

    my success key is working now need to tweak url

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
 // links I may need:
-// https://en.wikipedia.org/wiki/Special:Random
-// https://www.mediawiki.org/wiki/API:Main_page
-// https://en.wikipedia.org/wiki/Special:ApiSandbox#action=query&titles=Main%20Page&prop=revisions&rvprop=content&format=jsonfm
+// var url = "https://en.wikipedia.org/wiki/Special:Random"
+// var url =  "https://www.mediawiki.org/wiki/API:Main_page"
+var url = "https://en.wikipedia.org/w/api.php?action=query&titles=Main%20Page&prop=revisions&rvprop=content&format=json"
+// var url = "https://en.wikipedia.org/wiki/Special:ApiSandbox#action=query&titles=Main%20Page&prop=revisions&rvprop=content&format=jsonfm"
 
 function getIt(){
-  console.log("it works");
+  console.log("getIt works");
   $.ajax({
-    
-     url: "something",
+     url: url,
 
      success: function(result){
+       console.log("Below is RESULT: ");
        console.log(result);
          }})
 
