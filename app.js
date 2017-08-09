f01f957a52757c4009779ebfd677d7c10753deb8
Ara <ayaghsizian@gmail.com>
2017-08-09 15:53:19 -0400

    working on ajax url

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-alert(2);
+// links I may need:
+// https://en.wikipedia.org/wiki/Special:Random
+// https://www.mediawiki.org/wiki/API:Main_page
+// https://en.wikipedia.org/wiki/Special:ApiSandbox#action=query&titles=Main%20Page&prop=revisions&rvprop=content&format=jsonfm
+
+function getIt(){
+  console.log("it works");
+  $.ajax({
+    
+     url: "something",
+
+     success: function(result){
+       console.log(result);
+         }})
+
+
+}
