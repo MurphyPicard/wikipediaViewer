6597dcb9ef263a766c69852693c59bd37e1f3d24
Ara <ayaghsizian@gmail.com>
2017-08-11 15:25:37 -0400

    both go and random buttons ajax calls work correctly

--- app.js
+++ app.js
@@ -1,23 +1,25 @@
 var article = $('article');
 var url = "https://en.wikipedia.org/w/api.php";
+var userInput = '';
+var rand = 1;
+var resultsLimit = 22;
+
 
 function getIt(){
   console.log("getIt works");
-  var userInput = $("#searchInput").val();
-
-
+  userInput = $("#searchInput").val();
   $.ajax({
     //  type: 'GET',
     //  dataType: 'json',
-    //  async: false,
+    // //  async: false,
     //  contentType: 'application/json; charset=UTF-8;',
     url: url,
-    data: { action: 'opensearch', search: userInput, format: 'json' },
+    data: { action: 'opensearch', search: userInput, format: 'json', limit: resultsLimit },
     success: function(res){
       console.log("Below is RESULT: ");
       console.log(res);
       article.empty();
-      for(var i = 0; i < 10; i++){
+      for(var i = 0; i < resultsLimit; i++){
         article.append( '<div class="articles">' + (i+1) +
                        '<p id="titles">' + res[1][i] + '</p><br>' +
                        '<p id="descriptions">' + res[2][i] + '</p><br>' +
@@ -37,15 +39,25 @@ function getIt(){
               //        console.log('Error: ' + data);
               //    });
 }//getIt
+
+// gets one random article using the users word
 function getRandom(){
+  rand = Math.floor(Math.random() * resultsLimit);
+  userInput = $("#searchInput").val();
   $.ajax({
-    url: 'https://en.wikipedia.org/wiki/Special:Random',
-    data: { action: 'opensearch', search: userInput, format: 'json' },
-
+    url: url,
+    data: { action: 'opensearch', search: userInput, format: 'json', limit: resultsLimit },
     success: function(res){
-      url: url,
-      console.log(res);
 
+      console.log("Below is Random: ");
+      console.log(res);
+      article.empty();
+      article.append( '<div class="articles">' +
+                     '<p id="titles">' + res[1][rand] + '</p><br>' +
+                     '<p id="descriptions">' + res[2][rand] + '</p><br>' +
+                     '<a href="' + res[3][rand] + '">' + res[3][rand] + '</a>'
+                     + '</div><hr>'
+                   );//append
     }//success
-  })
+  })//ajax
 }//getRandom
